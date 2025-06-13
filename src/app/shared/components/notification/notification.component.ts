7ea855643ad1b26eb80f2a2282e693811a1f2922
SilvandenBraber <info@silvandenbraber.nl>
2025-06-13 19:14:32 +0200

    Created the variations of notification component + props

--- src/app/shared/components/notification/notification.component.ts
+++ src/app/shared/components/notification/notification.component.ts
@@ -1,5 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  warningOutline,
+  checkmarkCircleOutline,
+  informationCircleOutline,
+  alertCircleOutline,
+  megaphoneOutline,
+} from 'ionicons/icons';
+
+addIcons({
+  'warning-outline': warningOutline,
+  'checkmark-circle-outline': checkmarkCircleOutline,
+  'information-circle-outline': informationCircleOutline,
+  'alert-circle-outline': alertCircleOutline,
+  'megaphone-outline': megaphoneOutline,
+});
+
+export enum NotificationType {
+  Warning = 'warning',
+  Success = 'success',
+  Info = 'info',
+  Error = 'error',
+  Coach = 'coach',
+}
 
 @Component({
   selector: 'app-notification',
@@ -9,7 +33,28 @@ import { IonIcon } from '@ionic/angular/standalone';
   imports: [IonIcon],
 })
 export class NotificationComponent implements OnInit {
+  inline = input<boolean>(false);
+  type = input<NotificationType>(NotificationType.Success);
+  showAction = input<boolean>(true);
+
   constructor() {}
 
   ngOnInit() {}
+
+  getIconName(): string {
+    switch (this.type()) {
+      case NotificationType.Warning:
+        return 'warning-outline';
+      case NotificationType.Success:
+        return 'checkmark-circle-outline';
+      case NotificationType.Info:
+        return 'information-circle-outline';
+      case NotificationType.Error:
+        return 'alert-circle-outline';
+      case NotificationType.Coach:
+        return 'megaphone-outline';
+      default:
+        return 'checkmark-circle-outline';
+    }
+  }
 }
